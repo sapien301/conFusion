5695e9f29df6f88b54a389d431709290b0d1c1dc
root <root@localhost.localdomain>
2020-10-25 13:01:53 +0530

    Week 2

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Leader} from  '../shared/leader';
+import {LeaderService} from '../services/leader.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+leaders:Leader[];
+
+selectedLeader=Leader;
+
+  constructor(private leaderService:LeaderService) { }
+
+  ngOnInit() {
+    this.leaders = this.leaderService.getLeaders();
+  }
+
+  onSelect(leader:Leader){
+    this.selectedLeader=leader;
+}
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,28 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
+
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -26,10 +43,15 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
 
   ],
-  providers: [],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
